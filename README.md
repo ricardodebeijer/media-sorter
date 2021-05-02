0b34319fc861c4d317312b704fd61c1ac6862ab4
Ricardo de Beijer <ricardo.de.beijer@ict.nl>
2021-05-02 13:28:14 +0200

    Added arguments

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Sorts from SRC into DEST, using YEAR/MONTH folder structure.
 Separate top level folders for:
 -  Documents (pdf,docx,pptx etc)
 -  Media (jpg,png,mp4 etc)
+
+
+
+Example use for Documents:
+Src folder: `Z:/SourceTest/` (forward slash will be added if not provided)
+Dest folder: `Z:/DestTest/Documents/` (forward slash will be added if not provided)
+Extensions to move: `.pdf .docx .doc .pptx .xlsx .xls .txt`
+
+Full command:
+`python sortfiles.py "Z:/SourceTest/" "Z:/DestTest/Documents/" --extensions .pdf .docx .doc .pptx .xlsx .xls .txt`
+
+
+For media (photos and videos):
+Src folder: `Z:/SourceTest/` (forward slash will be added if not provided)
+Dest folder: `Z:/DestTest/Media/` (forward slash will be added if not provided)
+Extensions to move: `.png .jpg .jpeg`
+
+Full command:
+`python sortfiles.py "Z:/SourceTest/" "Z:/DestTest/Media/" --extensions .png .jpg .jpeg`
--- sortfiles.py
+++ sortfiles.py
@@ -2,19 +2,33 @@
 import os, time, shutil, sys
 from pathlib import Path
 
-def main():
-    # Do end with /
-    srcDir = 'Z:/SourceTest/'
-    # Dont end with /
-    destDir = 'Z:/DestTest/Documenten/'
-    extensions = ('.pdf','.docx','.doc','.pptx','.rtf','.xlsx','.xls','.txt','.dwg')
+def sortFiles(
+    srcDir,
+    destDir,
+    extensions
+):
+    print('Moving files matching', extensions, 'from', srcDir , 'to', destDir);
+    # Check if srcDir exists
+    if not os.path.exists(srcDir):
+        raise Exception("Source directory does not exist")
+    
+    # Add forward slash to srcDir if not existing
+    if not srcDir.endswith('/'):
+        srcDir = srcDir + '/'
+        print('Forward slash added to srcDir')
+    # Add forward slash to destDir if not existing
+    if not destDir.endswith('/'):
+        destDir = srcDir + '/'
+        print('Forward slash added to destDir')
+
+    nothingMoved = True;
     # List all files in SRC
     for f in os.listdir(srcDir):
         srcPath = srcDir + f
         # Check if its a file
         if os.path.isfile(srcPath):
             # Check if its in the extension list
-            if f.lower().endswith(extensions):
+            if f.lower().endswith(tuple(extensions)):
                 # Get modified time
                 ftime = time.gmtime(os.path.getmtime(srcPath))
 
@@ -36,8 +50,39 @@ def main():
                 # Move file to destPath
                 shutil.move(srcPath, destPath);
 
+                # Set flag something was moved
+                nothingMoved = False
                 # Log
                 print('"',srcPath, '" modified at:', time.strftime('%Y-%m-%d', ftime),'moved to: "', destPath,'"')
+
+    if nothingMoved:
+        print('Nothing was matches/moved.')
+def main():
+    import argparse
+    # setup command line parsing
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Sort files into folders by date",
+    )
+    parser.add_argument("src_dir", type=str, help="source directory")
+    parser.add_argument("dest_dir", type=str, help="destination directory")
+    # parser.add_argument("-r", "--recursive", action="store_true", help="search src_dir recursively")
+    parser.add_argument(
+        "--extensions",
+        type=str,
+        nargs="+",
+        default=[],
+        help="a list of extensions to match",
+    )
+
+    # parse command line arguments
+    args = parser.parse_args()
+
+    sortFiles(
+        args.src_dir,
+        args.dest_dir,
+        args.extensions,
+    )
 
 if __name__ == "__main__":
     main()
